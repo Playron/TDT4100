28e7553d6c6ddd0baf594b9b413279ba89cc7b28
per_h <per_h@DESKTOP-KUVCIKP>
2018-02-23 16:37:57 +0100

    Øving 5: Card del 2

---
+++ src/Oving5/Card.java
@@ -0,0 +1,46 @@
+package Oving5;
+
+public class Card {
+	
+	char type;
+	int cardValue;
+	
+	public Card(char type, int cardValue) {
+		if(!(type == 'S' || type == 'H' || type == 'D' || type == 'C')) {
+			throw new IllegalArgumentException("Type må være enten S, H, D eller C");
+		}
+		else {
+			this.type = type;
+		}
+		
+		if(cardValue > 13 || cardValue <= 0) {
+			throw new IllegalArgumentException("Kortverdi må være mellom 0 og 13");
+			}else {
+				this.cardValue = cardValue;
+		}
+	}
+	
+	public char getSuit() {
+		return this.type;
+	}
+	
+	public int getFace() {
+		return this.cardValue;
+	}
+	
+	
+	public String toString() {
+		return "" + getSuit() + getFace();
+	}
+	
+	
+	
+
+	public static void main(String[] args) {
+		Card c1 = new Card('S', 0);
+		
+	System.out.println(c1);
+
+	}
+
+}
---
+++ src/Oving5/CardHand.java
@@ -0,0 +1,49 @@
+package Oving5;
+
+import java.util.ArrayList;
+
+public class CardHand {
+	
+	ArrayList<Card> kort;
+	
+	public CardHand(){
+		kort= new ArrayList<Card>();
+	}
+	
+	public int getCardCount() {
+		return kort.size();
+	}
+	
+	public Card getCard(int n) {
+		return kort.get(n);
+	}
+	
+	public void addCards(Card kortet) {
+		if(!(kort.contains(kortet))) {
+			kort.add(kortet);
+		}else {
+			throw new IllegalArgumentException("Kortet finner allerede!");
+		}
+	}
+	
+	public Card play(int n) {
+		Card spillekort = kort.get(n);
+		kort.remove(spillekort);
+		return spillekort;
+	}
+	
+
+	public static void main(String[] args) {
+		CardHand hand = new CardHand();
+		Card c1 = new Card('S', 10);
+		Card c2 = new Card('S', 11);
+		Card c3 = new Card('H', 11);
+		hand.addCards(c1);
+		hand.addCards(c2);
+		hand.play(1);
+		hand.addCards(c3);
+		
+
+	}
+
+}
