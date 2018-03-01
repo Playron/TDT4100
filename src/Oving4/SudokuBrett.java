8cd160ae653772378f45bd58eb8471e2822bc616
perdh <perdh@stud.ntnu.no>
2018-03-01 16:59:19 +0100

    c

--- src/Oving4/SudokuBrett.java
+++ src/Oving4/SudokuBrett.java
@@ -35,24 +35,40 @@ public class SudokuBrett {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Hvilken kolonne vil du plassere tallet? ");
 		rad = sc.nextInt();
-		System.out.println("Hvilken rad vil du plassere tallet p�?");
+		System.out.println("Hvilken rad vil du plassere tallet p�?");
 		kol = sc.nextInt();
 		System.out.println("Hvilket tall vil du plassere?");
 		tall = sc.nextInt();
+		if(tall <=9) {
 		this.brett[rad][kol] = tall;
 		sc.close();
-	}
+			}else {
+				System.out.println("Tallet må være mellom 0 og 9");
+				plasserTall();
+			}
+		}
+	
+	
+	public boolean checkValidMove() {
+		for(int i = 0; i < brett[i].length; i++) {
+			while(brett[i] != brett[i+1]) {
+				System.out.println("Hele raden må ha 9 distinkte tall fra 1-9");
+				return false;
+				}
+			}
+		}
 	
 	
 	
 	public static void main(String[] args) {
 		int[][] brett = new int[9][9];
 		SudokuBrett b1 = new SudokuBrett(brett);
-		brett[1][2] = 3;
 		printBoard(brett);
 		b1.plasserTall();
 		printBoard(brett);
-		
+		b1.plasserTall();
+		printBoard(brett);
+		b1.checkValidMove();
 	}
 	
 	
